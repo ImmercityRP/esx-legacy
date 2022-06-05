554e629c96cb70401af4846ae5e0b441006dfa03
ImmercityRP <immercityrp@gmail.com>
2022-06-05 18:33:03 -0400

    money to bank

--- [esx_addons]/esx_society/server/main.lua
+++ [esx_addons]/esx_society/server/main.lua
@@ -68,7 +68,7 @@ AddEventHandler('esx_society:withdrawMoney', function(societyName, amount)
 		TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function(account)
 			if amount > 0 and account.money >= amount then
 				account.removeMoney(amount)
-				xPlayer.addMoney(amount)
+				xPlayer.addAccountMoney("bank", amount)
 				xPlayer.showNotification(_U('have_withdrawn', ESX.Math.GroupDigits(amount)))
 			else
 				xPlayer.showNotification(_U('invalid_amount'))
@@ -88,7 +88,7 @@ AddEventHandler('esx_society:depositMoney', function(societyName, amount)
 	if xPlayer.job.name == society.name then
 		if amount > 0 and xPlayer.getMoney() >= amount then
 			TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function(account)
-				xPlayer.removeMoney(amount)
+				xPlayer.removeAccountMoney("bank", amount)
 				xPlayer.showNotification(_U('have_deposited', ESX.Math.GroupDigits(amount)))
 				account.addMoney(amount)
 			end)
