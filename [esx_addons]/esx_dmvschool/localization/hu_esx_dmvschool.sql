f9d544fa04da3a173b4c35c633ad81e979b032e9
ImmercityRP <immercityrp@gmail.com>
2022-06-05 17:50:08 -0400

    more cleanup

--- [esx_addons]/esx_dmvschool/localization/hu_esx_dmvschool.sql
+++
@@ -1,8 +0,0 @@
-USE `es_extended`;
-
-INSERT INTO `licenses` (`type`, `label`) VALUES
-	('dmv', 'Vezetési engedély'),
-	('drive', 'Jogosítvány'),
-	('drive_bike', 'Motoros Jogosítvány'),
-	('drive_truck', 'Kereskedelmi Jogosítvány')
-;
--- [esx_addons]/esx_dmvschool/localization/si_esx_dmvschool.sql
+++
@@ -1,8 +0,0 @@
-USE `es_extended`;
-
-INSERT INTO `licenses` (`type`, `label`) VALUES
-	('dmv', 'CPP Licenca'),
-	('drive', 'Licenca za voznjo avtomobila'),
-	('drive_bike', 'Licenca za voznjo motorja'),
-	('drive_truck', 'Licenca za voznjo tovornjaka')
-;
